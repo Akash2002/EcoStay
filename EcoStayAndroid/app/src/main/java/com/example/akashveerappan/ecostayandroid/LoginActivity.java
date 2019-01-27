f2b502c44faeb05fa796a0a265e070da487890d9
Akash Veerappan <akashpalaniappan@gmail.com>
2019-01-26 22:16:25 -0500

    Installed MDC 2

--- EcoStayAndroid/app/src/main/java/com/example/akashveerappan/ecostayandroid/LoginActivity.java
+++ EcoStayAndroid/app/src/main/java/com/example/akashveerappan/ecostayandroid/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.example.akashveerappan.ecostayandroid;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
     }
+
+
 }
